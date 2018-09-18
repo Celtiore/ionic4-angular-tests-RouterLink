9adafb5faca4289c579507ac3906246a2e0e69b3
Celtiore <projetpos.projetpos@gmail.com>
2018-09-18 21:13:17 +0200

    readme

--- README.md
+++ README.md
@@ -29,21 +29,13 @@ Combine ion-tabs and split pane
 # Tests
 origin : ionic blank starter
 
-    -   1/ Home -> Single page with back button : OK
-    -   2/ Home -> First Tab1 with 2 tabs : OK
-    -   3/ Home -> Second Tab 2 with 2 tabs : OK
-    -   4/ Home -> First Split Pane 1 with 2 pages : OK
-
-    -   4*/ Inside Split Pane 1 -> Tabs 3
-    -       but only footer is shown
-
-    -   5/ Home -> Second Split Pane with 2 pages : OK
-
+    -   Use 2 differents <ion-split-pane>
+    -   Inside each <ion-split-pane> use 2 differents <ion-tabs>
+    -   Need to use components in <ion-tab>
+    -   if someone have suggestions or changes, you're welcome
+    
 ##
 
-I need help to configure correctly 4*/ and verify my Routes.
-
-thanks you
 
 You can Try on Stackblitz : 
 
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,7 @@ const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
   { path: '', loadChildren: './pages/configuration/split-configuration/split-configuration.module#SplitConfigurationPageModule' },
-  //{ path: 'page1', loadChildren: './pages/administration/page1/page1.module#Page1PageModule' },
-  //{ path: 'page2', loadChildren: './pages/administration/page2/page2.module#Page2PageModule' },
   { path: '', loadChildren: './pages/administration/split-administation/split-administation.module#SplitAdministationPageModule' },
-  //{ path: 'admin2', loadChildren: './pages/administration/admin2/admin2.module#Admin2PageModule' },
-  //{ path: 'admin1', loadChildren: './pages/administration/admin1/admin1.module#Admin1PageModule' },
-  //{ path: '', loadChildren: './pages/administration/tab-administration/tab-administration.module#TabAdministrationPageModule' },
-  //  { path: 'tabTest', loadChildren: './pages/configuration/tab-test/tab-test.module#TabTestPageModule' },
 ];
 
 @NgModule({
